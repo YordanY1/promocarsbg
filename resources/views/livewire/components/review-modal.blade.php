5a23725869139ea2cf0178e65e6858e6ca254e9d
YordanYordanov <yordanyordanov0104@gmail.com>
2025-03-13 01:10:38 +0200

    Update review-modal.blade.php

--- resources/views/livewire/components/review-modal.blade.php
+++ resources/views/livewire/components/review-modal.blade.php
@@ -3,10 +3,13 @@
         <div class="fixed inset-0 flex items-center justify-center z-50">
             <div class="w-full max-w-3xl p-6 relative bg-transparent">
 
-
-                <button wire:click="closeModal" class="absolute top-3 right-3 text-gray-300 hover:text-white text-3xl">
+                <button wire:click="closeModal"
+                    class="absolute top-2 right-2 md:top-4 md:right-4 w-10 h-10 flex items-center justify-center
+                bg-gray-800 bg-opacity-50 text-white text-2xl rounded-full hover:bg-gray-900 transition z-50">
                     ✕
                 </button>
+
+
 
                 <div class="bg-white bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg p-8 text-center">
 
@@ -28,12 +31,13 @@
                     </div>
 
                     <div class="mt-6">
-                        <button wire:click="closeModal"
+                        <button type="button" wire:click="closeModal"
                             class="bg-[#b01e45] text-white px-6 py-3 rounded-lg text-lg w-full md:w-auto
-                            hover:bg-[#9a1b3d] transition">
+                            hover:bg-[#9a1b3d] transition focus:outline-none">
                             Затвори
                         </button>
                     </div>
+
 
                 </div>
             </div>
